0525e2a46e8b686d47b881f48bf2d0bde09e1371
Sharp0024 <155568922+Sharp0024@users.noreply.github.com>
2024-11-19 16:37:25 +0700

    Update knn.py

--- knn.py
+++ knn.py
@@ -9,13 +9,13 @@ st.header('kairung')
 col1, col2, col3 = st.columns(3)
 with col1:
  st.header("Versicolor")
-st.image("./img/iris1.jpg")
+ st.image("./img/iris1.jpg")
 with col2:
  st.header("Verginiga")
-st.image("./img/iris2.jpg")
+ st.image("./img/iris2.jpg")
 with col3:
  st.header("Setosa")
-st.image("./img/iris3.jpg")
+ st.image("./img/iris3.jpg")
 html_7 = """
 <div style="background-color:#EC7063;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
 <center><h5>สถิติข้อมูลดอกไม้</h5></center>
@@ -48,7 +48,7 @@ pt_len = st.slider("กรุณาเลือกข้อมูล petal.lengt
 pt_wd = st.slider("กรุณาเลือกข้อมูล petal.width")
 sp_len = st.number_input("กรุณาเลือกข้อมูล sepal.length")
 sp_wd = st.number_input("กรุณาเลือกข้อมูล sepal.width")
- if st.button("ทำนายผล"):
+if st.button("ทำนายผล"):
   #st.write("ทำนาย")
  dt = pd.read_csv("./data/iris-3.csv")
  X = dt.drop('variety', axis=1)
@@ -65,4 +65,4 @@ sp_wd = st.number_input("กรุณาเลือกข้อมูล sepal.
  else:
   st.image("./img/iris3.jpg")
 else:
-st.write("ไม่ทำนาย")
+ st.write("ไม่ทำนาย")
